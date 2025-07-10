a32d94c633a6bf941086a1e44bcb34db35ba0c41
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2025-07-10 18:25:45 +0500

    Refactor ChooseUs component to include image columns and update Partner component to match new layout

--- app/components/ChooseUs.tsx
+++ app/components/ChooseUs.tsx
@@ -1,31 +1,60 @@
 import React from "react";
 import ChooseUsCard from "./ChooseUsCard";
+import Image from "next/image";
 
 const ChooseUs = () => {
   return (
     <section className="container mx-auto my-[100px]">
       <h4 className="mb-5">Why Choose Us</h4>
       <h2 className="text-center mb-12">Your Trusted Real Estate Partner</h2>
-      <ChooseUsCard
-        number={1}
-        title="Verified Listings Only"
-        description={`Lorem ipsum dolor sit\namet,`}
-      />
-      <ChooseUsCard
-        number={2}
-        title="Transparent Pricing"
-        description={`Lorem ipsum dolor sit\namet,`}
-      />
-      <ChooseUsCard
-        number={1}
-        title="Expert Local Agents"
-        description={`Lorem ipsum dolor sit\namet,`}
-      />
-      <ChooseUsCard
-        number={1}
-        title="Full Legal Support"
-        description={`Lorem ipsum dolor sit\namet,`}
-      />
+      <div className="flex justify-between">
+        {/* Left Column: 40% width */}
+        <div className="w-[38%] grid grid-cols-2 gap-5">
+          <ChooseUsCard
+            number={1}
+            title="Verified Listings Only"
+            description={`Lorem ipsum dolor sit\namet,`}
+          />
+          <ChooseUsCard
+            number={2}
+            title="Transparent Pricing"
+            description={`Lorem ipsum dolor sit\namet,`}
+          />
+          <ChooseUsCard
+            number={3}
+            title="Expert Local Agents"
+            description={`Lorem ipsum dolor sit\namet,`}
+          />
+          <ChooseUsCard
+            number={4}
+            title="Full Legal Support"
+            description={`Lorem ipsum dolor sit\namet,`}
+          />
+        </div>
+
+        {/* Right Column: 50% width */}
+        <div className="w-[50%] flex flex-col">
+          {/* First Image */}
+          <div
+            className="w-full h-[297px] rounded-[30px] bg-cover bg-no-repeat bg-center"
+            style={{
+              backgroundImage: "url('/assets/images/choose-2.png')",
+              backgroundColor: "lightgray",
+            }}
+          ></div>
+
+          {/* Second Image */}
+          <div
+            className="w-full h-[213px] rounded-[30px] bg-no-repeat"
+            style={{
+              backgroundImage: "url('/assets/images/choose-1.png')",
+              backgroundColor: "lightgray",
+              backgroundPosition: "0px -29.058px",
+              backgroundSize: "100% 165.782%",
+            }}
+          ></div>
+        </div>
+      </div>
     </section>
   );
 };
