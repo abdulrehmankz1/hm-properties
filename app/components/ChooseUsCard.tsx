a230341ecb3aabb4f0d9fb32d0e01e34107257eb
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2025-07-10 17:24:48 +0500

    Refactor ChooseUsCard component: update styles and dimensions

--- app/components/ChooseUsCard.tsx
+++ app/components/ChooseUsCard.tsx
@@ -8,13 +8,11 @@ type ChooseUsCardProps = {
 
 function ChooseUsCard({ number, title, description }: ChooseUsCardProps) {
   return (
-    <div className="p-7 bg-[#F5F7F9] rounded-3xl w-fit choosUsCard">
+    <div className="p-7 bg-[#F5F7F9] rounded-3xl w-[265px] h-[245px] mb-5 choosUsCard">
       <div className="py-1.5 px-4 bg-white text-xl font-medium w-fit rounded-full">
         {number}
       </div>
-      <div className="text-xl font-medium text-black mt-[70px]">
-        {title}
-      </div>
+      <div className="text-xl font-medium text-black mt-[70px]">{title}</div>
       <div className="text-[#00000080] mt-1 whitespace-pre-line">
         {description}
       </div>
