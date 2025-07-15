6424906ddb0a3339fb533b6f49fca0331816a8fe
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2025-07-15 10:47:14 +0500

    `Refactor ChooseUsCard component: add flexbox layout and reorganize child elements`

--- app/components/ChooseUsCard.tsx
+++ app/components/ChooseUsCard.tsx
@@ -6,15 +6,33 @@ type ChooseUsCardProps = {
   description: string;
 };
 
+// function ChooseUsCard({ number, title, description }: ChooseUsCardProps) {
+//   return (
+//     <div className="p-7 bg-[#F5F7F9] rounded-3xl w-[100%] md:h-[245px] h-[200px] choosUsCard">
+//       <div className="py-1.5 px-4 bg-white w-fit rounded-full text-2xl">
+//         {number}
+//       </div>
+//       <div className="text-black md:mt-[70px] mt-[20px] text-2xl">{title}</div>
+//       <div className="text-[#00000080] mt-1 whitespace-pre-line text-base">
+//         {description}
+//       </div>
+//     </div>
+//   );
+// }
 function ChooseUsCard({ number, title, description }: ChooseUsCardProps) {
   return (
-    <div className="p-7 bg-[#F5F7F9] rounded-3xl w-[100%] md:h-[245px] h-[200px] choosUsCard">
+    <div className="p-7 bg-[#F5F7F9] rounded-3xl w-full h-[200px] md:h-[245px] flex flex-col">
+      {/* Number Badge */}
       <div className="py-1.5 px-4 bg-white w-fit rounded-full text-2xl">
         {number}
       </div>
-      <div className="text-black md:mt-[70px] mt-[20px] text-2xl">{title}</div>
-      <div className="text-[#00000080] mt-1 whitespace-pre-line text-base">
-        {description}
+
+      {/* Title and Description Wrapper */}
+      <div className="flex-1 flex flex-col justify-end mt-[20px] md:mt-[25px]">
+        <div className="text-black text-2xl">{title}</div>
+        <div className="text-[#00000080] mt-1 whitespace-pre-line text-base">
+          {description}
+        </div>
       </div>
     </div>
   );
